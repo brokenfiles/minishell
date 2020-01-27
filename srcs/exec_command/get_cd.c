2fa78798bc0aaf2e36724c5659b5f03823e4e4bf
Max BRIGNOLI <mbrignol@e2r9p13.42.fr>
2020-01-27 16:39:37 +0100

    cdcdcd

--- srcs/exec_command/get_cd.c
+++ srcs/exec_command/get_cd.c
@@ -4,7 +4,11 @@
 int		get_cd(t_data *data)
 {
 	int index;
-
+	if (!(getcwd(data->cwd, sizeof(data->cwd))))
+		return (0);
+	printf("%s\n", data->cwd);
+	if (chdir("~/Cursus") == -1)
+	printf("%s\n", data->cwd);
 	index = 0;
 	ft_putstr("CDCD\n");
 	return (1);
