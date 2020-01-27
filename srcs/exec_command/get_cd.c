4a6d9c5ebf5cf5779191456d0707858a3f6ff8ea
Max BRIGNOLI <mbrignol@e2r9p13.42.fr>
2020-01-27 17:00:38 +0100

    cd

--- srcs/exec_command/get_cd.c
+++ srcs/exec_command/get_cd.c
@@ -1,15 +1,30 @@
 
 #include "../../includes/minishell.h"
 
+int		get_split_len(char **split)
+{
+	int	i;
+
+	i = 0;
+	while (split[i])
+		i++;
+	return (i);
+}
+
 int		get_cd(t_data *data)
 {
-	int index;
-	if (!(getcwd(data->cwd, sizeof(data->cwd))))
+	char	**arguments;
+
+	arguments = ft_split(data->arguments, ' ');
+	if (get_split_len(arguments) != 1)
+	{
+		ft_putstr("too many arguments.\n");
 		return (0);
-	printf("%s\n", data->cwd);
-	if (chdir("~/Cursus") == -1)
-	printf("%s\n", data->cwd);
-	index = 0;
-	ft_putstr("CDCD\n");
+	}
+	if (chdir(arguments[0]) == -1)
+	{
+		ft_putstr("this directory does not exists.\n");
+		return (0);
+	}
 	return (1);
 }
