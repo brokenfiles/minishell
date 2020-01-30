caa2c0f259e4ec452bbc51320d8713c54ba09ccd
Brokenfiles <louis.nevysup@gmail.com>
2020-01-30 21:34:29 +0100

    before WEEEKEND

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -1,12 +1,18 @@
 
 #include "../includes/minishell.h"
 
+void	cancel()
+{
+	ft_printf("\n\n");
+}
+
 int	main(int ac, char **av, char **env)
 {
 	t_data	*data;
 
 	data = init_struct(env);
 	ft_printf(PRE_LINE, ((get_last_char(data->cwd, '/') != -1 && ft_strlen(data->cwd) != 1) ? data->cwd + get_last_char(data->cwd, '/') + 1 : data->cwd));
+	signal(SIGINT, );
 	while (get_next_line(0, &(data->line)) > 0)
 	{
 		replace_env(data);
