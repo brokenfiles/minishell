4d15cd4fb5ee53840303197d21690b6d47caba81
brokenfiles <louis.nevysup@gmail.com>
2020-01-30 23:29:55 +0100

    adding files

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -1,26 +1,27 @@
 
 #include "../includes/minishell.h"
 
+t_data	*g_data;
+
 void	cancel()
 {
-	ft_printf("\n\n");
+	ft_putchar('\n');
+	ft_printf(PRE_LINE, ((get_last_char(g_data->cwd, '/') != -1 && ft_strlen(g_data->cwd) != 1) ? g_data->cwd + get_last_char(g_data->cwd, '/') + 1 : g_data->cwd));
 }
 
 int	main(int ac, char **av, char **env)
 {
-	t_data	*data;
-
-	data = init_struct(env);
-	ft_printf(PRE_LINE, ((get_last_char(data->cwd, '/') != -1 && ft_strlen(data->cwd) != 1) ? data->cwd + get_last_char(data->cwd, '/') + 1 : data->cwd));
-	signal(SIGINT, );
-	while (get_next_line(0, &(data->line)) > 0)
+	g_data = init_struct(env);
+	ft_printf(PRE_LINE, ((get_last_char(g_data->cwd, '/') != -1 && ft_strlen(g_data->cwd) != 1) ? g_data->cwd + get_last_char(g_data->cwd, '/') + 1 : g_data->cwd));
+	signal(SIGINT, cancel);
+	while (get_next_line(0, &(g_data->line)) > 0)
 	{
-		replace_env(data);
-		parse_line(data);
-		free(data->line);
-		data->line = NULL;
-		ft_printf(PRE_LINE, ((get_last_char(data->cwd, '/') != -1 && ft_strlen(data->cwd) != 1) ? data->cwd + get_last_char(data->cwd, '/') + 1 : data->cwd));
+		replace_env(g_data);
+		parse_line(g_data);
+		free(g_data->line);
+		g_data->line = NULL;
+		ft_printf(PRE_LINE, ((get_last_char(g_data->cwd, '/') != -1 && ft_strlen(g_data->cwd) != 1) ? g_data->cwd + get_last_char(g_data->cwd, '/') + 1 : g_data->cwd));
 	}
-	free(data->line);
+	free(g_data->line);
 	return (EXIT_SUCCESS);
 }
