460996ddbbf5bae5ac49b4d15cbb5d2263a81ae9
Max BRIGNOLI <mbrignol@e2r10p23.42.fr>
2020-01-31 11:05:52 +0100

    export bientot fini

--- srcs/exec_command/command_export.c
+++ srcs/exec_command/command_export.c
@@ -25,15 +25,25 @@ int		add_export(t_data *data, char *export)
 	int i;
 	int len;
 	char **new_env;
+
 	len = ft_bigstrlen(data->env);
 	i = 0;
-
 	if (!(new_env = malloc(sizeof(char*) * (len + 2))))
 		return (0);
 	while (data->env[i])
-		new_env = ft_strdup(data->env[i++]);
+	{
+		new_env[i] = ft_strdup(data->env[i]);
+		i++;
+	}
+	new_env[i++] = export;
+	new_env[i] = 0;
+	free_splitted(data->env, 0);
+	data->env = new_env;
 	return (1);
 }
+
+//SI DEJA PRESENT REMPLACER
+//TRIM GUILLEMET
 
 int		get_export(t_data *data)
 {
@@ -44,10 +54,7 @@ int		get_export(t_data *data)
 	while (data->arguments[x])
 	{
 		if ((export = get_only_export_var(data, x)) != NULL)
-		{
 			add_export(data, export);
-
-		}
 		x++;
 	}
 	return (1);
