478ac34b3b50a6bb592e2e610d684d9e255cf6d5
Max BRIGNOLI <mbrignol@e2r2p15.42.fr>
2020-02-15 02:02:48 +0100

    test

--- srcs/parsing/manage_redirection.c
+++ srcs/parsing/manage_redirection.c
@@ -20,65 +20,22 @@ int		redirect_output(t_redirect redirect)
 	return (fd);
 }
 
-int		redirect_input(void)
-{
-	struct stat	buff;
-	int			fd;
-	int			ret;
+//void		redirect_input(int fd)
+//{
+//	struct stat	buff;
+//	int			ret;
 
-	ret = stat("out", &buff);
-	if (ret == -1 || S_ISDIR(buff.st_mode))
-		return (EXIT_FAILURE);
-	if (!(fd = open("out", O_RDONLY)))
-		return (-1);
-	if (dup2(fd, STDIN_FILENO) < 1)
-		return (-1);
-	return (fd);
-}
+//	ret = stat("out", &buff);
+//	if (ret == -1 || S_ISDIR(buff.st_mode))
+//		return (EXIT_FAILURE);
+//	if (!(fd = open("out", O_RDONLY)))
+//		return (-1);
+//	if (dup2(fd, STDIN_FILENO) < 1)
+//		return (-1);
+//	return (fd);
+//}
 
 int		exec_hub(t_data *data)
 {
-	/*int fds[2];
-	fds[0] = dup(1);
-	fds[1] = open("out", O_RDWR);
-	dup2(fds[1], 1);
 
-	run_command(data, exec_prog);
-
-	dup2(fds[0], 1);
-	close(fds[0]);
-	close(fds[1]);*/
-
-	char **split;
-	char **cmds;
-	int stds[1];
-	int fd;
-	int i;
-
-	i = 0;
-	split = ft_split(data->line, '|');
-	while (split[i])
-	{
-		if (split[i + 1] == 0)
-		{
-			cmds = ft_split_spec(split[i], ' ');
-			data->command = ft_strdup(cmds[0]);
-			run_command(data, cmds);
-		}
-		else
-		{
-			cmds = ft_split_spec(split[i], ' ');
-			data->command = ft_strdup(cmds[0]);
-			stds[0] = dup(0);
-			fd = redirect_input();
-			run_command(data, cmds);
-			stop_redirect(fd, stds[0], STDIN_FILENO);
-			close(stds[0]);
-		}
-		free_splitted(cmds, 0);
-		free(data->command);
-		i++;
-	}
-	free_splitted(split, 0);
-	return (EXIT_SUCCESS);
 }
